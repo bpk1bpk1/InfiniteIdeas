42e6e050cfe3b7092aa470a7304e8f63a48fab55
= <sriksrin@iu.edu>
2016-11-02 17:58:26 -0400

    added list of ideas

--- src/main/java/com/infiniteideas/WebSecurityConfig.java
+++ src/main/java/com/infiniteideas/WebSecurityConfig.java
@@ -34,6 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .and()
                 .logout()
                     .permitAll();
+        http.csrf().disable();
     }
 
     @Autowired
